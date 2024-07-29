8ab52b889b6dc972ad110ecbbfb7ce7914c5a0f7
jigong2024 <choijming21@naver.com>
2024-07-29 22:16:43 +0900

    grid 사용해보기

--- index.js
+++ index.js
@@ -27,34 +27,6 @@ function createMovieCard(movie) {
   return card;
 }
 
-// function createMovieCard(movie) {
-//   const card = document.createElement('div');
-//   card.className = 'movie-card';
-//   card.innerHTML = `
-//     <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-//     <h3>${movie.title}</h3>
-//     <p>${movie.overview}</p>
-//     <span>Rating: ${movie.vote_average}</span>
-//   `;
-//   card.addEventListener('click', () => alert(`Movie ID: ${movie.id}`));
-//   return card;
-// }
-
-// 검색 기능 구현
-// document.getElementById("search-button").addEventListener("click", () => {
-//   const query = document.getElementById("search-input").value.toLowerCase();
-//   const movieCards = document.querySelectorAll(".movie-card");
-//   movieCards.forEach((card) => {
-//     const title = card.querySelector("h3").textContent.toLowerCase();
-//     // console.log(title);
-//     if (title.includes(query)) {
-//       card.style.display = "block";
-//     } else {
-//       card.style.display = "none";
-//     }
-//   });
-// });
-
 document.getElementById("search-button").addEventListener("click", () => {
   const searchInput = document.getElementById("search-input");
   const query = searchInput.value.trim().toLowerCase();
